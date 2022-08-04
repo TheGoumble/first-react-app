50289adf33dbe20adcbe1060a39149921b3ef6b0
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-08-04 14:09:27 -0400

    making the click property

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
-  let count = 0;
+  //let count = 0;
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>My first React App</h1>
-        <p>
-         You clicked the button {count} times.
-        </p>
-        <button onClick={() => {
-          console.log("I clicked the button");
-          count++;
-        }}>Click Me</button>
+        <p>You clicked the button {count} times.</p>
+        <button
+          onClick={() => {
+            console.log("I clicked the button " + count)
+            // count++
+            setCount(count + 1)
+          }}
+        >
+          Add One
+        </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
